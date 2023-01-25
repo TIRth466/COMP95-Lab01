c9e33589634255246e48083d7f3c8909ceea41dd
Tirth4 <122820497+Tirth4@users.noreply.github.com>
2023-01-24 23:27:05 -0500

    Modified both files in the repo clone
    -Deleted a print() statement from script1.py
    -Added two print() statement to script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,5 @@
 # Print lines froom Zen of Python
 print("Beautiful is better than ugly.")
 print("Explicit is better than implicit.")
+print("if the implementation is hard to explain, it's a bad idea.")
+print("if the implementation is easy to explain, it may be a good idea.")
